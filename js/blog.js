9e10b77b0588245d9a43982528117510077becc3
wxzgis <wxzgis@163.com>
2020-02-04 19:30:24 +0800

    博客模板 文本格式

--- js/blog.js
+++ js/blog.js
@@ -1,5 +1,49 @@
 this.onload = () => {
-    new Vue({
+
+    Vue.component('wxz-title', {
+        data: function(){
+            return {};
+        },
+        props: ['content', 'time'],
+        template: '<div><div class="jumbotron p-3 p-md-5 text-white rounded bg-dark"><div class="col-md-6 px-0"><h1 class="display-4 font-italic">{{content}}</h1></div></div><el-divider content-position="left">{{time}}</el-divider></div>'
+    });
+    Vue.component('wxz-subtitle', {
+        data: function (){
+            return {};
+        },
+        props: ['content'],
+        template: '<h2 class="blog-post-subtitle">{{content}}</h2>'
+    });
+    Vue.component('wxz-cshape', {
+        data: function(){
+            return {};
+        },
+        props: ['content'],
+        template: '<span>// {{content}}</span> '
+    });
+    Vue.component('wxz-html', {
+        data: function(){
+            return {};
+        },
+        props: ['content'],
+        template: '<span>&lt;!-- {{content}} --&gt;</span> '
+    });
+    Vue.component('wxz-css', {
+        data: function(){
+            return {};
+        },
+        props: ['content'],
+        template: '<span>/* {{content}} */</span> '
+    });
+    Vue.component('wxz-python', {
+        data: function(){
+            return {};
+        },
+        props: ['content'],
+        template: '<span># {{content}}</span> '
+    });
+
+    const vm = new Vue({
         el: '#app',
         data(){
             return {
