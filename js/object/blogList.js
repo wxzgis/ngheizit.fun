f00d4a513d0bc835301699f17c060c91e1d8215d
wxzgis <wxzgis@163.com>
2020-02-06 15:07:30 +0800

    博客 在CentOS下安装Node及Vue和Express相关框架

--- js/object/blogList.js
+++ js/object/blogList.js
@@ -1,8 +1,13 @@
 const blogList = [
     {
         title: "起点",
-        img: './blog/2020-02-06/糊涂塌客.png',
-        url: './blog/2020-02-06.html',
+        img: './blog/2020-02-06-1/糊涂塌客.png',
+        url: './blog/2020-02-06-1.html',
         time: '2020年2月6日 11点51分'
+    },{
+        title: '在CentOS下安装Node及Vue和Express相关脚骨架',
+        img: './blog/2020-02-06-2/修改host.jpg',
+        url: './blog/2020-02-06-2.html',
+        time: '2020年2月6日 12点35分'
     }
-]
+].reverse();
