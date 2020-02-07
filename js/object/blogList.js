feee4d657920db0db93e2cff219c205299876638
wxzgis <wxzgis@163.com>
2020-02-07 11:44:46 +0800

    博客 在Centos中配置Git

--- js/object/blogList.js
+++ js/object/blogList.js
@@ -9,5 +9,10 @@ const blogList = [
         img: './blog/2020-02-06-2/修改host.jpg',
         url: './blog/2020-02-06-2.html',
         time: '2020年2月6日 12点35分'
+    },{
+        title: '在Centos中配置Git',
+        img: './blog/2020-02-07-1/git.png',
+        url: './blog/2020-02-07-1.html',
+        time: '2020年2月7日 11点41分'
     }
 ].reverse();
